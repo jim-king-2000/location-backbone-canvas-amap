04f532a67a698342585a9413e0ab84ec763a8dd2
Jim <jim_king_2000@163.com>
2019-09-18 17:55:26 +0800

    refactory

--- src/CanvasTracks.js
+++ src/CanvasTracks.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import { Polyline } from 'react-amap';
 import { lineStyle, colorPool } from 'location-backbone-canvas';
-import HalBMap from '../hal/HalBMap';
 
-export const CanvasTracks = ({ tracks, __map__ }) => {
-  const halMap = new HalBMap(__map__);
-  return (
-    <>
-      {tracks && tracks.map(track => {
-        let path = track.splittedTrack || [];
-        if (!Array.isArray(path) || !Array.isArray(path[0])) {
-          path = [path];
-        }
-        return path.map((pathItem, i) => <Polyline
-          __map__={__map__}
-          key={`${track.thingId}-${i}`}
-          path={pathItem}
-          events={{ created: () => halMap.setFitView() }}
-          style={{
-            strokeColor: colorPool[track.colorIndex % colorPool.length] ||
-              '#006600',
-            ...lineStyle
-          }} />);
-      })}
-    </>
-  );
-};
+export const CanvasTracks = ({ tracks, __map__ }) => (
+  <>
+    {tracks && tracks.map(track => {
+      let path = track.splittedTrack || [];
+      if (!Array.isArray(path) || !Array.isArray(path[0])) {
+        path = [path];
+      }
+      return path.map((pathItem, i) => <Polyline
+        __map__={__map__}
+        key={`${track.thingId}-${i}`}
+        path={pathItem}
+        style={{
+          strokeColor: colorPool[track.colorIndex % colorPool.length] ||
+            '#006600',
+          ...lineStyle
+        }} />);
+    })}
+  </>
+);
