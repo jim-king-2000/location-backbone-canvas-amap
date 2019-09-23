bcfafed21e525bbb9a26eb989973087e7b35bea1
Jim <jim_king_2000@163.com>
2019-09-23 16:50:24 +0800

    refactory

--- src/CanvasMapStyle.js
+++ src/CanvasMapStyle.js
@@ -6,7 +6,7 @@ export class CanvasMapStyle extends Component {
     const __map__ = this.props.__map__;
     return <Select
       options={['全图', '底图']}
-      value={__map__.getFeatures() === ['bg'] ? '全图' : '底图'}
+      value={__map__.getFeatures() === ['bg'] ? '底图' : '全图'}
       onChange={({ option }) => {
         console.log(option, __map__.getFeatures())
         __map__.setFeatures(option === '全图' ?
